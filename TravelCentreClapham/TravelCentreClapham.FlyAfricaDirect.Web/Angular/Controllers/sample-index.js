410848147840b6c83a649b4b48536e2973bbcc88
unknown <ZhangM@C2628.moneycorp.local>
2016-08-31 16:31:10 +0100

    angular script

---
+++ TravelCentreClapham/TravelCentreClapham.FlyAfricaDirect.Web/Angular/Controllers/sample-index.js
@@ -0,0 +1,30 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('nexusCore.Web').controller('sample', function($scope, apiCall, dataConverter, dialogs) {
+        $scope.init = function() {
+            //apiCall([
+            //    { url: "/ModelBuilder/Customer" }
+            //]).then(function(data) {
+            //    $scope.enquiryForm = data[0];
+            //});
+            $scope.enquiryForm = {
+                FirstName: "",
+                LastName: ""
+            };
+        };
+
+        $scope.submit = function() {
+            //apiCall([
+            //    { postMethod: "POST", url: "/Enquiry", data: { customer: $scope.enquiryForm } }
+            //]).then(function(data) {
+            //    $scope.result = data[0];
+            //    $scope.success();
+            //});
+        };
+
+        $scope.success = function() {
+            
+        }
+    });
+})();
---
+++ TravelCentreClapham/TravelCentreClapham.FlyAfricaDirect.WebApi/Controllers/ModelBuilderController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+using TravelCentreClapham.FlyAfricaDirect.Dal.Models;
+
+namespace TravelCentreClapham.FlyAfricaDirect.WebApi.Controllers
+{
+    public class ModelBuilderController : ApiController
+    {
+        public CustomerDto Customer()
+        {
+            return new CustomerDto();
+        }
+    }
+}
