6823866fba251650c3ad05810d1d67280974979f
unknown <ZhangM@C2628.moneycorp.local>
2016-09-02 15:09:06 +0100

    client validation

--- TravelCentreClapham/TravelCentreClapham.FlyAfricaDirect.Web/Angular/Controllers/sample-index.js
+++ TravelCentreClapham/TravelCentreClapham.FlyAfricaDirect.Web/Angular/Controllers/sample-index.js
@@ -3,15 +3,17 @@
 
     angular.module('nexusCore.Web').controller('sample', function($scope, apiCall, dataConverter, dialogs) {
         $scope.init = function() {
+            $scope.enquiryForm = {
+                value: {},
+                error: {}
+            };
             //apiCall([
             //    { url: "/ModelBuilder/AfricanWebsiteOffer01" }
             //]).then(function(data) {
             //    $scope.enquiryForm = data[0];
             //});
-            $scope.enquiryForm.error = {
-                firstNameRequired: false,
-                lastNameRequired: false
-            };
+
+            $scope.resetValidation();
         };
 
         $scope.submit = function() {
@@ -21,16 +23,32 @@
             //    $scope.result = data[0];
             //    $scope.success();
             //});
+            if ($scope.validate()) {
+                console.log("good");
+            }
         };
 
         $scope.validate = function () {
-
-            if (angular.isUndefined($scope.enquiryForm.FirstName))
+            $scope.resetValidation();
+            var isValid = true;
+            if (angular.isUndefined($scope.enquiryForm.value.FirstName)) {
                 $scope.enquiryForm.error.firstNameRequired = true;
-            if (angular.isUndefined($scope.enquiryForm.LastName))
-                $scope.enquiryForm.error.firstNameRequired = true;
+                isValid = false;
+            }
+            if (angular.isUndefined($scope.enquiryForm.value.LastName)) {
+                $scope.enquiryForm.error.lastNameRequired = true;
+                isValid = false;
+            }
 
+            return isValid;
         };
+
+        $scope.resetValidation = function() {
+            $scope.enquiryForm.error = {
+                    firstNameRequired: false,
+                    lastNameRequired: false
+            };
+        }
 
         $scope.success = function() {
             
